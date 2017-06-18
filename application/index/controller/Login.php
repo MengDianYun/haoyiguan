baefcb40c3c95a9f49a0d5b01a74b0998583fa01
Jianle Chen <q690712575@live.com>
2017-06-19 07:46:55 +0800

    改进

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -10,17 +10,39 @@ namespace app\index\controller;
 
 
 use think\Controller;
+use think\Db;
 
 class Login extends  Controller
 {
     //显示登陆页面
-    public  function  showLoginPage(){
+    public  function  showLoginPage ()   :string  {
 
-        return $this->fetch('',['title'=>'登陆']);
+       return $this->fetch('',['title'=>'登陆']);
     }
 
-    public function  login(){
-        return 'POSTtest';
+
+
+    public  function  login() :int{
+        $username=$this->request->param('username');
+        $password=$this->request->param('password');
+        $isAjax=$this->request->isAjax();
+        //判断如果不是ajax就返回0
+        if (!$isAjax){
+            return 0;
+        }
+        $res=Db::table('admin_user')->where('username',$username)->where('password',$password)->find();
+        if ($res==null){
+            return 0;
+        }else{
+            $this->loged($res);
+            return 1;
+        }
+
+
+    }
+
+    private function loged(array $user){
+
     }
 
 }
---
+++ application/index/model/AdminUser.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by haoyiguan.
+ * User: lele
+ * Date: 2017/6/9 0009
+ * Time: 15:25
+ * 萌点云科技（深圳）有限公司
+ */
+
+namespace app\index\model;
+
+
+use think\Model;
+
+class AdminUser extends Model
+{
+
+}
